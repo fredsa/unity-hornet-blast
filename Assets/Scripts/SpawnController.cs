06ed13c44fb4bf66754bb1ae37fcfda85d5f13bb
Fred Sauer <fredsa@google.com>
2015-06-26 10:32:37 -0500

    enemies flying along waypoints

--- Assets/Scripts/SpawnController.cs
+++ Assets/Scripts/SpawnController.cs
@@ -7,9 +7,12 @@ public class SpawnController : MonoBehaviour {
 	public float delay = 0f;
 	public float rate = 1f;
 
+	LayerMask layermask;
 	bool needSpawn = false;
+	float checkRadius;
 
 	void Start () {
+		layermask = LayerMask.GetMask("EnemiesEffector", "EnemiesKinematic");
 		InvokeRepeating ("Spawn", delay, rate);
 	}
 	
@@ -18,10 +21,30 @@ public class SpawnController : MonoBehaviour {
 	}
 
 	void Update() {
-		if (needSpawn) {
-			needSpawn = false;
-			GameObject clone = Instantiate (prefab, transform.position, transform.rotation) as GameObject;
-			clone.transform.parent = gameObject.transform;
+		if (!needSpawn) {
+			return;
+		}
+
+		if (checkRadius > 0f) {
+			bool collision = Physics2D.OverlapCircleAll (transform.position, checkRadius, layermask).Length > 1;
+			if (collision) {
+				Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + Vector2.one * checkRadius, Color.red);
+				Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position - Vector2.one * checkRadius, Color.blue);
+//				Debug.Log (Physics2D.OverlapCircle (transform.position, checkRadius));
+				return;
+			}
+		}
+//		Debug.Log (Physics2D.OverlapCircle (transform.position, checkRadius));
+		
+		needSpawn = false;
+		GameObject clone = Instantiate (prefab, transform.position, transform.rotation) as GameObject;
+		clone.transform.parent = gameObject.transform;
+
+		if (checkRadius == 0f) {
+			CircleCollider2D collider = clone.GetComponentInChildren<CircleCollider2D> () as CircleCollider2D;
+			Vector2 cloneScale = ((Vector2)clone.transform.localScale);
+//			Debug.Log(collider.radius + " * " + cloneScale.magnitude + " (" + cloneScale + ")");
+			checkRadius = collider.radius * cloneScale.magnitude / 2;
 		}
 	}
 }
---
+++ Assets/Scripts/WaypointController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaypointController : MonoBehaviour {
+
+	public GameObject waypoints;
+	public float speed = 1f;
+
+	Rigidbody2D rb;
+	int targetWaypointIndex = -1;
+	Vector2 targetPosition;
+	Vector2 targetDirection;
+
+	void Start () {
+		rb = GetComponentInChildren<Rigidbody2D> ();
+		nextTarget();
+	}
+
+	void nextTarget() {
+		targetWaypointIndex = ++targetWaypointIndex % waypoints.transform.childCount;
+		targetPosition = waypoints.transform.GetChild (targetWaypointIndex).position;
+	}
+
+	void Update() {
+		Vector2 direction = targetPosition - rb.position;
+		float distance = direction.magnitude;
+		if (distance < .1f) {
+			nextTarget ();
+		}
+		direction.Normalize ();
+		targetDirection = direction * speed;
+
+		rb.velocity = Vector3.Slerp (rb.velocity, targetDirection, distance/10f);
+
+//		rb.position = Vector2.Lerp(rb.position, targetPosition, 3f * Time.deltaTime);
+		
+//		rb.position = Vector2.Lerp(rb.position, targetPosition, 3f * Time.deltaTime);
+
+//		Vector2 velocity = (Vector2)rb.velocity;
+//		Vector2.SmoothDamp (rb.position, targetPosition, ref velocity, .2f);
+//		rb.velocity = velocity;
+	}
+}
