c7b9c883cc422e68b80fbb9a0e6af941fc8813e8
Fred Sauer <fredsa@google.com>
2015-06-30 17:20:10 -0500

    separate score text object

---
+++ Assets/Scripts/ScoreTextController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreTextController : MonoBehaviour {
+
+	int score;
+	Text scoreText;
+	Animator animator;
+
+	void Start () {
+		score = 0;
+	    scoreText = gameObject.GetComponent<Text> ();
+	    animator = gameObject.GetComponent<Animator> ();
+		UpdateScore ();
+	}
+
+	public void AddPoints(int points) {
+		score += points;
+		animator.SetTrigger ("PointsAdded");
+		UpdateScore ();
+	}
+
+	void UpdateScore () {
+		scoreText.text = string.Format ("{0,4:n0}", score);
+	}
+
+	void Update () {
+	}
+}
