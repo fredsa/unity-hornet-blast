7e4bd7247119197657f07e87523434c673d09ab3
Fred Sauer <fredsa@google.com>
2015-07-01 10:37:18 -0500

    prefab and annimation

---
+++ Assets/PlayerColliderController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class PlayerColliderController : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Enemy")) {
+			GameObject clone = Instantiate(explosionPrefab, other.transform.position, other.transform.rotation) as GameObject;
+			TextMesh mesh = clone.GetComponentInChildren<TextMesh>();
+			mesh.text = "";
+
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
+	}
+	
+}
