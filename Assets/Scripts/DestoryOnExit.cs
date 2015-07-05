f1306bf74607bc8d162af312a545b66462b10691
Fred Sauer <fredsa@google.com>
2015-07-04 22:00:24 -0500

    better game boundary

---
+++ Assets/Scripts/DestoryOnExit.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class DestoryOnExit : MonoBehaviour {
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Bullet") || other.gameObject.CompareTag ("Enemy")) 
+		{
+			Destroy (other.gameObject);
+		}
+	}
+	
+}
